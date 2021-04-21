8e8ab051de0b2e4490517e9c3d42301b62ebcd40
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-04-21 13:57:37 +0100

    Updated version + pipelines

--- .scripts/build.bat
+++ .scripts/build.bat
@@ -1,1 +1,1 @@
-"C:\Program Files\Unity\Hub\Editor\2020.2.2f1\Editor\Unity.exe" -batchmode -quit -nographics -projectPath ./Game/ -executeMethod BuildHelper.PerformBuild -logFile build.log
+"C:\Program Files\Unity\Hub\Editor\2021.1.3f1\Editor\Unity.exe" -batchmode -quit -nographics -projectPath ./Game/ -executeMethod BuildHelper.PerformBuild -logFile build.log
