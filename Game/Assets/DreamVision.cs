9378f095e7b541f2524fa31c547cf26262fe9ed6
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-04-25 21:59:53 +0100

    Updated dreamvision to get all materials using shader

--- Game/Assets/DreamVision.cs
+++ Game/Assets/DreamVision.cs
@@ -1,24 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class DreamVision : MonoBehaviour
 {
-    private Material _material;
+    private IEnumerable<Material> _materials;
     public Transform _player;
 
+    [SerializeField]
     [Range(0f, 1f)]
-    public float Percentage = 0f;
+    private float percentage = 0f;
+    public float Percentage
+    {
+        get { return percentage; }
+        set
+        {
+            percentage = Mathf.Clamp01(value);
+        }
+    }
 
     void Awake()
     {
-        _material = GetComponent<SpriteRenderer>().material;
+        _materials = FindMaterials();
+    }
+
+    private IEnumerable<Material> FindMaterials()
+    {
+        var materials = Resources.FindObjectsOfTypeAll<Material>();
+
+        return materials.Where(p => p.shader.name == "Shader Graphs/Dreamworld");
     }
 
     // Update is called once per frame
     void Update()
     {
-        _material.SetVector("PlayerPos", _player.transform.position);
-        _material.SetFloat("Percentage", Percentage);
+        foreach (var material in _materials)
+        {
+            material.SetVector("PlayerPos", _player.transform.position);
+            material.SetFloat("Percentage", Percentage);
+        }
+
+        // Set any NPCs within a radius of the player (based on percentage) to be enemies.
     }
 }
