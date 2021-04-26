9b2cbaca20fabab773bf6c032dc13856e34fec0d
Jeremy Sapsed <jeremy@sapsed.co.uk>
2021-04-26 11:40:59 +0100

    Added character diversity

--- Game/Assets/Scripts/Player/PlayerController.cs
+++ Game/Assets/Scripts/Player/PlayerController.cs
@@ -11,16 +11,14 @@ public class PlayerController : CharacterController
     {
         Inventory = GetComponent<CharacterInventory>();
     }
-    
-    // Start is called before the first frame update
-    void Start()
+
+    protected override void Start()
     {
-        
+        base.Start();
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void Update()
     {
-        
+        base.Update();
     }
 }
